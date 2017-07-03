633f36a4ee133af21b92f54d34269fcb6c4500e9
Francois BRUNHES aka fanfan <francois.brunhes@gmail.com>
2017-07-03 18:51:27 +0200

    Add disk metrics

--- metrics.go
+++ metrics.go
@@ -6,14 +6,45 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
 )
 
 //Metrics represents a basic struct with systems stats
 type Metrics struct {
-	CPUStats  []cpu.TimesStat        `json:"cpustats"`
+	//CPU
+	CPUStats []cpu.TimesStat `json:"cpustats"`
+	//Memory
 	MemStats  *mem.VirtualMemoryStat `json:"memstats"`
 	SwapStats *mem.SwapMemoryStat    `json:"swapstats"`
+	//Disk
+	IOStats        []map[string]disk.IOCountersStat `json:"iostats"`
+	PartitionStats []disk.PartitionStat             `json:"partitionstats"`
+	UsageStats     []*disk.UsageStat                `json:"usagestats"`
+}
+
+func getDiskInfo() ([]map[string]disk.IOCountersStat, []disk.PartitionStat, []*disk.UsageStat, error) {
+	pstats, err := disk.Partitions(false)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("Error getting partition stats: %v", err)
+	}
+	var iostats []map[string]disk.IOCountersStat
+	var usstats []*disk.UsageStat
+	for _, partition := range pstats {
+		iostat, err := disk.IOCounters(partition.Device)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("Error getting io stats for %s: %v", partition.Device, err)
+		}
+		iostats = append(iostats, iostat)
+		usstat, err := disk.Usage(partition.Device)
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("Error getting usage stats for %s: %v", partition.Device, err)
+		}
+		usstats = append(usstats, usstat)
+		//partitions = append(partitions, partition.Device)
+		//fmt.Println(disk.IOCounters(partition.Device))
+	}
+	return iostats, pstats, usstats, nil
 }
 
 func getSWAPInfo() (*mem.SwapMemoryStat, error) {
@@ -54,9 +85,16 @@ func (agent *Agent) basicMetrics() {
 		if err != nil {
 			log.Println(err)
 		}
+		io, ps, us, err := getDiskInfo()
+		if err != nil {
+			log.Println(err)
+		}
 		metrics.CPUStats = c
 		metrics.MemStats = m
 		metrics.SwapStats = s
+		metrics.IOStats = io
+		metrics.PartitionStats = ps
+		metrics.UsageStats = us
 		time.Sleep(3 * time.Second)
 	}
 }
