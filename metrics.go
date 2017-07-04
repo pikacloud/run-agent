b9a386221b0ee7af15e0dcc704c5cef23e73b5d6
Francois BRUNHES aka fanfan <francois.brunhes@gmail.com>
2017-07-04 16:50:42 +0200

    Add load average metrics

--- metrics.go
+++ metrics.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
 )
 
@@ -18,24 +19,42 @@ type Metrics struct {
 	MemStats  *mem.VirtualMemoryStat `json:"memstats"`
 	SwapStats *mem.SwapMemoryStat    `json:"swapstats"`
 	//Disk
-	IOStats        []map[string]disk.IOCountersStat `json:"iostats"`
-	PartitionStats []disk.PartitionStat             `json:"partitionstats"`
-	UsageStats     []*disk.UsageStat                `json:"usagestats"`
+	IOStats        map[string]disk.IOCountersStat `json:"iostats"`
+	PartitionStats []disk.PartitionStat           `json:"partitionstats"`
+	UsageStats     []*disk.UsageStat              `json:"usagestats"`
+	//Load Average
+	LoadAverage *load.AvgStat  `json:"loadaverage"`
+	MiscStats   *load.MiscStat `json:"miscstats"`
 }
 
-func getDiskInfo() ([]map[string]disk.IOCountersStat, []disk.PartitionStat, []*disk.UsageStat, error) {
+func getLoadAverage() (*load.AvgStat, *load.MiscStat, error) {
+	lastats, err := load.Avg()
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error getting load average information: %v", err)
+	}
+	msstats, err := load.Misc()
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error getting Misc information: %v", err)
+	}
+	return lastats, msstats, nil
+}
+
+func getDiskInfo() (map[string]disk.IOCountersStat, []disk.PartitionStat, []*disk.UsageStat, error) {
 	pstats, err := disk.Partitions(false)
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("Error getting partition stats: %v", err)
 	}
-	var iostats []map[string]disk.IOCountersStat
+	iostats, err := disk.IOCounters()
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("Error getting io stats for %v", err)
+	}
+	//var iostats []map[string]disk.IOCountersStat
 	var usstats []*disk.UsageStat
+	//fmt.Println(pstats)
 	for _, partition := range pstats {
-		iostat, err := disk.IOCounters(partition.Device)
-		if err != nil {
-			return nil, nil, nil, fmt.Errorf("Error getting io stats for %s: %v", partition.Device, err)
-		}
-		iostats = append(iostats, iostat)
+		//fmt.Println(partition)
+
+		//iostats = append(iostats, iostat)
 		usstat, err := disk.Usage(partition.Device)
 		if err != nil {
 			return nil, nil, nil, fmt.Errorf("Error getting usage stats for %s: %v", partition.Device, err)
@@ -89,12 +108,15 @@ func (agent *Agent) basicMetrics() {
 		if err != nil {
 			log.Println(err)
 		}
+		la, ms, err := getLoadAverage()
 		metrics.CPUStats = c
 		metrics.MemStats = m
 		metrics.SwapStats = s
 		metrics.IOStats = io
 		metrics.PartitionStats = ps
 		metrics.UsageStats = us
+		metrics.LoadAverage = la
+		metrics.MiscStats = ms
 		time.Sleep(3 * time.Second)
 	}
 }
